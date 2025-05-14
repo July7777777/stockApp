b401b879aa45fc499c68a4acf793d1234676fecc
July <1114174347@qq.com>
2025-05-15 02:49:31 +0800

    新增接口；新增精确计算decimal.js ；实时计算可用

--- api/apiXueQiu.js
+++ api/apiXueQiu.js
@@ -22,3 +22,14 @@ import {getX} from './http.js';
  *
  */
 export const kline = params => getX('/kline', params);
+
+/**
+ * 获取日基本信息
+ * https://stock.xueqiu.com/v5/stock/quote.json?symbol=SZ160125&extend=detail
+ * symbol: 股票或指数代码 必须
+ * extend: detail
+ * 指定了想要返回的数据指标。包括：
+ * detail: 日基本信息
+ *
+ */
+export const quote = params => getX('/quote', params);
--- pages/component/text/text.vue
+++ pages/component/text/text.vue
@@ -6,12 +6,39 @@
 				class="text-box"
 				scroll-y="true"
 			>
-				<text>{{ title }}</text>
+				<text>{{ title + ':' + getPremiumRate }}</text>
+
 			</view>
+			<uni-group
+				:title="SZ160125.name + ' - ' + SZ160125.exchange + SZ160125.code"
+				mode="card"
+			>
+				<view> 现&nbsp; &nbsp;价:{{ SZ160125.current }} {{ SZ160125.currency }} </view>
+				<view> 溢价率:{{ SZ160125.premium_rate }}% </view>
+			</uni-group>
+
+			<uni-group
+				:title="HSTECH.name + ' - ' + HSTECH.code"
+				mode="card"
+			>
+				<view
+					v-for="i, n in HSTECH.klines"
+					:key="n"
+				>{{ split(i) }} </view>
+			</uni-group>
+			<uni-group
+				:title="HSCGSI.name + ' - ' + HSCGSI.code"
+				mode="card"
+			>
+				<view
+					v-for="ii, nn in HSCGSI.klines"
+					:key="nn"
+				>{{ split(ii) }} </view>
+			</uni-group>
 			<view class="uni-btn-v">
 				<button
 					type="primary"
-					@click="stock"
+					@click="getData"
 				>add line</button>
 			</view>
 		</view>
@@ -19,35 +46,107 @@
 </template>
 <script>
 	import { stockKline, stockGet } from '@/api/apiTianTian.js';
-
+	import { quote } from '@/api/apiXueQiu.js';
+	import Decimal from 'decimal.js';
 	export default {
 		data () {
 			return {
 				title: '南方香港lof实时溢价估算',
-			}
-		},
-		methods: {
-			stock () {
-				let p = {
-					secid: '0.160125',
-				}
-				stockGet(p).then(res => {
-					console.log('res', res);
-				})
-			},
-			getData () {
-				let p = {
+				HS: {
 					klt: 101,
 					fqt: 1,
 					lmt: 2,
 					end: this.$dayjs().format('YYYYMMDD'),
 					fields1: 'f1,f2,f3,f4,f5,f6,f7,f8',
 					fields2: 'f51,f59',
-				}
-				Promise.all([stockKline({ ...p, secid: '124.HSCGSI' }), stockKline({ ...p, secid: '124.HSTECH' })]).then(res => {
-					console.log('res', res);
-				})
+				},
+				XG: {
+					symbol: 'SZ160125',
+					extend: 'detail'
+				},
+				SZ160125: {
+					exchange: "SZ",
+					code: "160125",
+					name: '南方香港LOF',//名称
+					current: '',//现价
+					premium_rate: '',//溢价率
+					currency: '',//单位
+				},
+				HSCGSI: {
+					code: "HSCGSI",
+					name: "恒生消费品制造及服务业指数",
+					klines: [],
+				},
+				HSTECH: {
+					code: "HSTECH",
+					name: "恒生科技指数",
+					klines: [],
+				},
+				timeInterval: null
 			}
+		},
+		computed: {
+			getPremiumRate () {
+				if (this.HSCGSI.klines.length && this.HSTECH.klines.length) {
+					let A = this.getD(this.HSCGSI.klines[0])
+					let B = this.getD(this.HSCGSI.klines[1])
+					let C = this.getD(this.HSTECH.klines[0])
+					let D = this.getD(this.HSTECH.klines[1])
+					let E = new Decimal(this.SZ160125.premium_rate)
+					let F = A.plus(B).plus(C).plus(D)
+					return F.div(2).minus(E).toFixed(2) + '%'
+				} else {
+					return '暂无数据'
+				}
+			}
+		},
+		methods: {
+			getData () {
+				Promise.all([stockKline({ ...this.HS, secid: '124.HSCGSI' }), stockKline({ ...this.HS, secid: '124.HSTECH' }), quote(this.XG)]).then(res => {
+					let [HSCGSI, HSTECH, SZ160125] = res;
+					console.log(HSCGSI, HSTECH, SZ160125);
+					this.HSCGSI = HSCGSI.data;
+					this.HSTECH = HSTECH.data;
+					this.SZ160125 = SZ160125.data.quote;
+				}).catch(err => {
+					clearInterval(this.timeInterval)
+					uni.showToast({
+						title: '请检查网络连接',
+						icon: 'error',
+						duration: 2000,
+					})
+					console.log(err)
+				})
+			},
+			split (str) {
+				if (str.length) {
+					let parts = str.split(",");
+					return `${parts[0]} →→→→→ ${parts[1]}%`
+				}
+				return '';
+			},
+			getD (str) {
+				if (str.length) {
+					let parts = str.split(",");
+					return new Decimal(parts[1])
+				}
+				return '';
+			},
+			init () {
+				this.timeInterval = setInterval(() => {
+					this.getData()
+				}, 5000)
+			}
+		},
+		onLoad () {
+			this.getData()
+			this.init()
+		},
+		onUnload () {
+			clearInterval(this.timeInterval)
+		},
+		onHide () {
+			clearInterval(this.timeInterval)
 		}
 	}
 </script>
